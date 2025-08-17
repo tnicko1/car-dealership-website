f30a79d68618c11c11b38514561944671ecde1de
Nikoloz Taturashvili <tnicko@proton.me>
2025-08-17 15:10:08 +0400

    feat: Update middleware config to include additional routes for account management

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,4 +19,4 @@ export default withAuth(
     }
 );
 
-export const config = { matcher: ["/admin/:path*"] };
+export const config = { matcher: ["/admin/:path*", "/account", "/my-listings", "/wishlist"] };
