7c36aa56aed35c2aa2302ecaf63e60b33f5c5e1e
Nikoloz Taturashvili <tnicko@proton.me>
2025-08-20 19:29:50 +0400

    refactor: soften gradient overlay in Hero section for increased subtlety

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,7 +22,7 @@ export default function HeroSection() {
                     priority
                 />
             </div>
-            <div className="absolute inset-0 bg-gradient-to-r from-red-900/40 to-red-700/30"></div>
+            <div className="absolute inset-0 bg-gradient-to-r from-red-900/20 to-red-700/10"></div>
 
             <div className="relative container mx-auto px-4 h-full flex flex-col justify-center">
                 <div>
